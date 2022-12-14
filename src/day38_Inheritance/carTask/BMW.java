9c9371d93a0a27a45ef502e302566d00bbddc894
Gokhan Serin <penmudamu@gmail.com>
2022-12-14 16:39:09 +0300

    codes completed for BMW

--- src/day38_Inheritance/carTask/BMW.java
+++ src/day38_Inheritance/carTask/BMW.java
@@ -1,7 +1,17 @@
 package day38_Inheritance.carTask;
 
-public class BMW {
+public class BMW extends Car {
 
+
+    public BMW(String model, int year, double price, String color, int miles) {
+        super("BMW", model, year, price, color, miles);
+    }
+    public void breaksDown(){
+        System.out.println(brand + " " + model + " is breaks down");
+    }
+    public void racing(){
+        System.out.println(brand + " " + model + " is racing");
+    }
 
 
 }
